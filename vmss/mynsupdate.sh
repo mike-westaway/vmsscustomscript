dfc03e590edadc5e3084a62c3b68f9b0de7cd6d9
Mike Westaway <miwestaw@microsoft.com>
2019-01-16 14:06:46 +0000

    hard coded example

---
+++ vmss/mynsupdate.sh
@@ -0,0 +1,7 @@
+scriptname="myscript.sh"
+echo "update delete termite04.mytermite.net a" > $scriptname
+echo "update add termite04.mytermite.net 3600 a 10.6.0.4" >> $scriptname
+echo "send" >> $scriptname
+
+./myscript.sh
+
